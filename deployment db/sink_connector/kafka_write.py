981cf69a13e9708276d5ecdefdc7dea79d22b7a8
MengseuThoeng <mengseu2004@gmail.com>
2025-01-30 14:04:08 +0700

    Fix IP Server

--- deployment db/sink_connector/kafka_write.py
+++ deployment db/sink_connector/kafka_write.py
@@ -2,7 +2,7 @@ from kafka import KafkaProducer
 import json
 from json import dumps
 
-p = KafkaProducer(bootstrap_servers = ['167.172.78.79:29093'], value_serializer = lambda x:dumps(x).encode('utf-8'))
+p = KafkaProducer(bootstrap_servers = ['202.178.125.77:29093'], value_serializer = lambda x:dumps(x).encode('utf-8'))
 
 data = {'name': 'roscoe'}
 
